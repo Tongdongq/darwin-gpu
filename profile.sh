cc70474f56bfc95d5250f3b4572a8cd37cfca044
T.D. (Tongdong) Qiu <tqiu@ce-cuda01.ewi.tudelft.nl>
2018-07-03 13:03:52 +0200

    added profile script

---
+++ profile.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+mode=${1:-1}
+T=${2:-1}
+BLOCKS=${3:-32}
+TPB=${4:-32}
+
+OUT="-o 28.nvprof"
+#OUT=
+
+last="$(head -n20 Makefile | grep "NCFLAGS" | grep -v "#")"
+if [[ $last == *"-lineinfo"* ]]; then
+	printf "Compiled ok\n"
+else
+        printf "Error: not compiled with -lineinfo\n\n"
+        exit 1
+fi
+
+if [[ "$mode" -ne "f" ]]; then
+	if [[ "$mode" -ne "nf" ]]; then
+		printf "Error: use [f|nf] for profiling mode as first argument\n\n"
+		exit 1
+	fi
+fi
+
+printf "Running with arguments $T $BLOCKS $TPB\n"
+if [[ "$mode" == "nf" ]] ; then
+	printf "Mode: non-file\n"
+else
+	printf "Mode: file\n"
+fi
+
+sleep 3
+
+METRICS="--metrics"
+M0=" gld_transactions,gst_transactions,gld_efficiency,gst_efficiency,gst_throughput,gld_throughput"
+M1=",gst_requested_throughput,gld_requested_throughput"
+M2=",l2_read_transactions,l2_write_transactions"
+M3=",shared_load_throughput,shared_store_throughput"
+
+EVENTS="--events"
+E0=" inst_executed,gld_inst_8bit,gld_inst_16bit,gld_inst_32bit,gld_inst_64bit"
+E1=",__l1_global_load_transactions,__l1_global_store_transactions"
+E2=",l1_local_load_hit,l1_local_load_miss,l1_local_store_hit,l1_local_store_miss"
+E3=",l2_subp0_read_sector_misses"
+E4=",l2_subp0_total_read_sector_queries"
+E5=",shared_load,shared_store,shared_load_replay,shared_store_replay"
+
+if [[ "$mode" == "nf" ]]; then
+	nvprof $METRICS$M0$M1$M2$M3$M4 $EVENTS$E0$E1$E2$E3$E4$E5  ./reference_guided reference.fasta reads.fasta $T $BLOCKS $TPB 
+else
+	nvprof --analysis-metrics $OUT ./reference_guided reference.fasta reads.fasta $T $BLOCKS $TPB
+fi
+
+

00ebab2b65f06ae2e432930458fd6a330813489e
T.D. (Tongdong) Qiu <tqiu@ce-cuda01.ewi.tudelft.nl>
2018-09-28 11:11:42 +0200

    added generateperfect.py script and generate PBSIM option

--- generate.sh
+++ generate.sh
@@ -7,14 +7,29 @@ G_SIZE=${4:-1}
 CROSSTALK=${5:-88}
 THRES=${6:-5}
 
+t=1
+
+if [[ "$1" == *"PBSIM"* ]]; then
+	cd ../PBSIM/src
+	./run.sh 8
+	./prepare_reads.py
+	t=0
+	printf "\nDon't forget to convert the PBSIM reads\n\n"
+fi
+
+if [ $t == 0 ]; then
+	exit 0
+fi
+
 if [ "$TACC" -eq "1" ]; then
 	src="$DATA/DAZZ_DB-master/human"
 	printf "Running on TACC\n"
+	cd $src
+	./generate $TYPE $LENGTH $N $G_SIZE $CROSSTALK $THRES
 else
 	src="../DAZZ_DB-master/human"
 	printf "Running on ce-cuda\n"
+	cd $src
+	./generate $TYPE $LENGTH $N $G_SIZE $CROSSTALK $THRES
 fi
-
-cd $src
-./generate $TYPE $LENGTH $N $G_SIZE $CROSSTALK $THRES
 

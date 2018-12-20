1199d219dc768de716ed2afdd61c2a27148cb89a
T.D. (Tongdong) Qiu <tqiu@ce-cuda01.ewi.tudelft.nl>
2018-12-20 16:42:15 +0100

    removed daligner measuring

--- measure_sensitivity_PBSIM.py
+++ measure_sensitivity_PBSIM.py
@@ -11,38 +11,21 @@ from operator import itemgetter
 def parse(line):
 	return map(int, re.findall('\d+', line))
 
-daligner = 0
-ref = 0
+ref = 0 # if ref is 1, reference-based alignment is assumed, otherwise de novo
 extra = 1 # if extra is 1, add the reverse of the AB overlap (BA), should increase sensitivity, and halve specificity
 remove_trivial = 1 # if equal to 1, removes all 'read X ovls with read X' overlaps
-recompute_tovls = 1 # if equal to 1, recomputes all theoretical overlaps based on the input files, otherwise, look for w_theoretical_overlaps
+recompute_tovls = 0 # if equal to 1, recomputes all theoretical overlaps based on the input files, otherwise, look for w_theoretical_overlaps
 ref_vs_read = 0 # if equal to 1, assume hovls are from ref vs read, otherwise from ref vs ref
 
 # thresholds to filter heuristic overlaps
-score_thres = 00
-min_length = 0
+score_thres = 600
+min_length = 990
 
-if len(sys.argv) > 1:
-	arg = sys.argv[1]
-	if arg == 'daligner':
-		print("Heuristic is Daligner")
-		daligner = 1
-	if arg == 'ref' or arg == 'reference':
-		print("Reference-based")
-		ref = 1
-if len(sys.argv) > 2:
-	arg = sys.argv[2]
-	if arg == 'daligner':
-		print("Heuristic is Daligner")
-		daligner = 1
-	if arg == 'ref' or arg == 'reference':
-		print("Reference-based")
-		ref = 1
 
-if daligner == 0:
-	print("Heuristic is Darwin")
 if ref == 0:
 	print("De-novo based")
+else:
+	printf("Reference based")
 
 # analyze all reads
 # data header: id, startpos in genome, length of overlap
@@ -52,24 +35,17 @@ if daligner == 0:
 		f2 = open('reads.fasta','r')
 	else:
 		f2 = open('reference.fasta','r')
-else:
-	f1 = open('../PBSIM/src/sd_0001.fasta','r')
-	if ref_vs_read == 1:
-		f2 = open('../PBSIM/src/sd_0002.fasta','r')
-	else:
-		f2 = open('../PBSIM/src/sd_0001.fasta','r')
+
 list1 = []		# contains info from f1
 list2 = []		# contains info from f2
 
 for line in f1:
 	if ">" in line:
-		#print(line)
 		t = parse(line)
 		list1.append(t)
 
 for line in f2:
 	if ">" in line:
-		#print(line)
 		t = parse(line)
 		list2.append(t)
 
@@ -121,16 +97,12 @@ if ref == 0:
 				o1 = max(a1, b1)
 				o2 = min(a2, b2)
 				ovl_length = o2 - o1
-				#print("r1: %d, r2: %d, a1: %d, a2: %d, b1: %d, b2: %d, o1: %d, o2: %d, length: %d" % (idx1, idx2, a1, a2, b1, b2, o1, o2, ovl_length))
 				sa1 = o1 - a1
 				sa2 = o2 - a1
 				sb1 = o1 - b1
 				sb2 = o2 - b1
-				#print("%d %d" % (sa2-sa1, sb2-sb1))
 				if ovl_length >= 1000:
 					all_theoretical_overlaps.append((idx1, idx2, sa1, sa2, sb1, sb2, 0))
-				#print("r1: %d, r2: %d, sa1: %d, sa2: %d, sb1: %d, sb2: %d" % (idx1, idx2, sa1, sa2, sb1, sb2))
-				#print()
 
 		print("Num theoretical ovls: %d" % len(all_theoretical_overlaps))
 
@@ -160,41 +132,22 @@ if remove_trivial == 1:
 ## ref_id, gen_pos, ovl_length, \
 ## read_id, gen_pos, ovl_length, \
 ## ref_start, ref_end, read_start, read_end, score, comp, 0
-## daligner:
-## ref_id, read_id, \
-## ref_start, ref_end, \
-## read_start, read_end, 0
 ## last 0 is used to find False Positives (FP)
 all_heuristic_overlaps = []
-if daligner == 0:
-	print('Reading darwin overlaps')
-	f1 = open('out.darwin')
-	for line in f1:
-		l = parse(line)
-		if len(l) < 10:
-			print('WARNING this darwin overlap does not have enough information')
-			print(line)
-			print(l)
-		l.append(0)
+print('Reading darwin overlaps')
+f1 = open('out.darwin')
+for line in f1:
+	l = parse(line)
+	if len(l) < 10:
+		print('WARNING this darwin overlap does not have enough information')
+		print(line)
+		print(l)
+	l.append(0)
+	all_heuristic_overlaps.append(l)
+	if extra == 1:
+		l = [l[3],l[4],l[5],l[0],l[1],l[2],l[8],l[9],l[6],l[7],l[10],l[11],0]
 		all_heuristic_overlaps.append(l)
-		if extra == 1:
-			l = [l[3],l[4],l[5],l[0],l[1],l[2],l[8],l[9],l[6],l[7],l[10],l[11],0]
-			all_heuristic_overlaps.append(l)
-	f1.close()
-else:
-	print('Reading daligner overlaps')
-	# daligner must have run already, with './run.sh 8 | tee >(grep "ids" > tmp) | grep -v "ids"'
-	# only consider ref_id and read_id for now, for coordinates, the commas need to be removed
-	f1 = open('../original/DALIGNER-old/tmp')
-	for line in f1:
-		l = parse(line)
-		if len(l) != 7:
-			print('WARNING this daligner overlap does not have enough information')
-			print(line)
-			print(l)
-		l.append(0)
-		all_heuristic_overlaps.append(l)
-	f1.close()
+f1.close()
 
 print("Num heuristic overlaps: %d" % len(all_heuristic_overlaps))
 
@@ -208,17 +161,8 @@ sa1 = 6; sa2 = 7; sb1 = 8; sb2 = 9
 
 if ref == 1:
 	min_length = 0
-	if daligner == 1:
-		last_idx = 999
-		sa1 = 2; sa2 = 3; sb1 = 4; sb2 = 5; score_idx = -1
-	else:
-		last_idx = 10
-		sa1 = 4; sa2 = 5; sb1 = 6; sb2 = 7; score_idx = 8
-else:
-	if daligner == 1:
-		hidx = 1
-		last_idx = 7
-		sa1 = 2; sa2 = 3; sb1 = 4; sb2 = 5; score_idx = 6
+	last_idx = 10
+	sa1 = 4; sa2 = 5; sb1 = 6; sb2 = 7; score_idx = 8
 
 
 if remove_trivial == 1:
@@ -226,11 +170,7 @@ if remove_trivial == 1:
 	print("Num non-trivial heuristic overlaps: %d" % len(all_heuristic_overlaps))
 
 all_heuristic_overlaps = [ovl for ovl in all_heuristic_overlaps if ovl[sa2]-ovl[sa1] >= min_length and ovl[sb2]-ovl[sb1] >= min_length]
-if score_idx == -1:
-	# daligner score = length ovl - diffs
-	all_heuristic_overlaps = [ovl for ovl in all_heuristic_overlaps if ovl[sa2] - ovl[sa1] - ovl[6] >= score_thres]
-else:
-	all_heuristic_overlaps = [ovl for ovl in all_heuristic_overlaps if ovl[score_idx] >= score_thres]
+all_heuristic_overlaps = [ovl for ovl in all_heuristic_overlaps if ovl[score_idx] >= score_thres]
 
 print("Filter: score_thres: %d, min_length: %d" % (score_thres, min_length))
 print("Num heuristic overlaps after filtering: %d" % len(all_heuristic_overlaps))
@@ -256,7 +196,6 @@ if ref == 0:
 		fn = 1
 		# start searching for matches where the ref_id matches
 		idx = bisect.bisect_left(tmp_list, tovl[0])
-		#print("ids %d %d, start idx: %d" % (tovl[0], tovl[1], idx))
 		while idx < len(all_heuristic_overlaps) and all_heuristic_overlaps[idx][0] == tovl[0]:
 			hovl = all_heuristic_overlaps[idx]
 			if tovl[0] == hovl[0] and tovl[1] == hovl[hidx]:
@@ -265,14 +204,15 @@ if ref == 0:
 			idx += 1
 		if fn == 1:
 			FN = FN + 1
+		"""else:
+			TP = TP + 1#"""
 	for hovl in all_heuristic_overlaps:
 		if hovl[last_idx] == 0:
 			FP = FP + 1
-			#print hovl
 		else:
-			TP = TP + 1
+			TP = TP + 1#"""
 else:
-	# fp is a list
+	# fp is a list with zeroes
 	## 0: read not mapped
 	## 1: read is mapped to the correct spot
 	fp = [0] * len(list2)
@@ -292,13 +232,12 @@ else:
 				maxScoreIdx = i
 		else:
 			t = all_heuristic_overlaps[maxScoreIdx]
-			#print("ID: %d, idx: %d, %s, %s" % (readID, maxScoreIdx, str(t), str(h)))
 			unique_hovls.append(all_heuristic_overlaps[maxScoreIdx])
 			readID = h[1]
 			maxScoreIdx = i
 			maxScore = -1
 
-	print("Hovls with unique readID: %d" % len(unique_hovls))#"""
+	print("Hovls with unique readID: %d" % len(unique_hovls))
 
 	for hovl in unique_hovls:
 	#for hovl in all_heuristic_overlaps:
@@ -319,20 +258,9 @@ else:
 			TP += 1
 		else:
 			FP += 1
-			#if FP < 50:
-			#	print hovl
 		fp[read_id] = 1
 
 	FN = len(list2) - sum(fp)
-
-	"""i = 0
-	for (idx,f) in enumerate(fp):
-		if f == 0:
-			if i > 100:
-				print list2[idx]
-			i += 1
-		if i > 110:
-			break#"""
 
 
 print("TP: %d" % TP)
